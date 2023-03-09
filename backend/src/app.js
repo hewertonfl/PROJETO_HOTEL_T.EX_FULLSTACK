044f4f1cb70eb0fdf9171c2486239ee5d3915cff
Hewerton <hewerton.fl@gmail.com>
2023-03-09 10:19:35 -0300

    testes

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,16 +8,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 (() => __awaiter(void 0, void 0, void 0, function* () {
-    const express = require('express');
-    const app = express();
+    const app = (0, express_1.default)();
     // const userModel = require('./api/models/UserModel.js')
-    //const consign = require('consign')
+    const consign = require('consign');
     const router = require('./api/routes/index.js');
     const port = '3000';
-    app.use(express.json());
+    app.use(express_1.default.json());
     app.use('/', router);
+    //consign().then('./api/middlewares/index.js').into(app)
     app.listen(port, () => {
         console.log(`Example app listening on port http://localhost:${port}`);
     });
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,14 @@
-import { Response, Request } from 'express'
+import express, { Express, Request, Response, Router } from 'express'
 ;(async () => {
-    const express = require('express')
-    const app = express()
+    const app: Express = express()
     // const userModel = require('./api/models/UserModel.js')
-    //const consign = require('consign')
+    const consign = require('consign')
     const router = require('./api/routes/index.js')
     const port = '3000'
 
     app.use(express.json())
     app.use('/', router)
+    //consign().then('./api/middlewares/index.js').into(app)
 
     app.listen(port, () => {
         console.log(`Example app listening on port http://localhost:${port}`)
