8ec38c78fad52d1380182f3733da37b1ee007f50
Hewerton <hewerton.fl@gmail.com>
2023-03-12 18:58:00 -0300

    Ajuste funções do vue

--- hotel_rc/frontend/src/views/admin/CadastrarAcomodacaoView.vue
+++ hotel_rc/frontend/src/views/admin/CadastrarAcomodacaoView.vue
@@ -1,69 +1,82 @@
 <template>
-        <form class="formulario" method="POST" @submit="this.cadastrarAcomodacao" enctype="multipart/form-data">
-            <label for="numero">Número:</label>
-            <input
+    <form class="formulario" enctype="multipart/form-data">
+        <label for="numero">Número:</label>
+        <input
             type="text"
             id="numero"
             placeholder="Digite o número da acomodação"
             v-model="numero"
             required
-            />
+        />
 
-            <label for="tipo">Tipo:</label>
-            <select v-model="tipo" id="tipo">
-                <option value="Suíte Luxuosa">Suíte Luxuosa</option>
-                <option value="Suíte com Varanda">Suíte com Varanda</option>
-                <option value="Quarto Superior">Quarto Superior</option>
-                <option value="Quarto Deluxe">Quarto Deluxe</option>
-                <option value="Quarto Executivo">Quarto Executivo</option>
-                <option value="Quarto Família">Quarto Família</option>
-                <option value="Quarto Adaptado para Deficientes">Quarto Adaptado para Deficientes</option>
-                <option value="Quarto com Vista para o Mar">Quarto com Vista para o Mar</option>
-                <option value="Quarto Econômico">Quarto Econômico</option>
-            </select>           
+        <label for="tipo">Tipo:</label>
+        <select v-model="tipo" id="tipo">
+            <option value="Suíte Luxuosa">Suíte Luxuosa</option>
+            <option value="Suíte com Varanda">Suíte com Varanda</option>
+            <option value="Quarto Superior">Quarto Superior</option>
+            <option value="Quarto Deluxe">Quarto Deluxe</option>
+            <option value="Quarto Executivo">Quarto Executivo</option>
+            <option value="Quarto Família">Quarto Família</option>
+            <option value="Quarto Adaptado para Deficientes">
+                Quarto Adaptado para Deficientes
+            </option>
+            <option value="Quarto com Vista para o Mar">
+                Quarto com Vista para o Mar
+            </option>
+            <option value="Quarto Econômico">Quarto Econômico</option>
+        </select>
 
-            <label for="descricao">Descrição:</label>
-            <textarea v-model="descricao" cols="30" rows="10"></textarea>
+        <label for="descricao">Descrição:</label>
+        <textarea v-model="descricao" cols="30" rows="10"></textarea>
 
-            <label for="preco">Preço:</label>
-            <input
+        <label for="preco">Preço:</label>
+        <input
             type="text"
             id="preco"
             placeholder="Digite o preço"
             v-model="preco"
             required
-            />
+        />
 
-            <label for="status">Status:</label>
-            <select v-model="status" id="status">
-                <option value="Livre">Livre</option>
-                <option value="Ocupado">Ocupado</option>
-            </select>             
+        <label for="status">Status:</label>
+        <select v-model="status" id="status">
+            <option value="Livre">Livre</option>
+            <option value="Ocupado">Ocupado</option>
+        </select>
 
-            <label for="foto">Imagem:</label>
-            <input type="file" @change="cadastrarImagem" name="foto" id="foto">  
+        <label for="foto">Imagem:</label>
+        <input
+            type="file"
+            ref="file"
+            @change="selectFile(), uploadImage($event)"
+            name="UploadImage"
+        />
 
-            <button>Cadastrar Acomodação</button>
-        </form>
+        <button type="button" @click="cadastrarAcomodacao()">
+            Cadastrar Acomodação
+        </button>
+    </form>
 </template>
 
 <script>
 import axios from 'axios'
+//import e from 'express'
 
-export default{
-    name: "CadastrarAcomodacaoView",
+export default {
+    name: 'CadastrarAcomodacaoView',
     data() {
-        return{
-           numero: null,
-           tipo: null,
-           descricao: null,
-           preco: null,
-           imagem: null,
-           status: null,
+        return {
+            numero: null,
+            tipo: null,
+            descricao: null,
+            preco: null,
+            imagem: 'aaa',
+            status: null,
+            file: '',
         }
     },
-    methods:{
-        cadastrarAcomodacao(){
+    methods: {
+        cadastrarAcomodacao: async function () {
             const dados = {
                 numero: this.numero,
                 tipo: this.tipo,
@@ -73,21 +86,36 @@ export default{
                 status: this.status,
             }
 
-            axios.post('http://localhost:3000/api/acomodacoes/', dados)
-            .then(response => console.log(response))
-            .catch(error => error)
+            await axios
+                .post('http://localhost:3000/admin/room/insert', dados)
+                .then((response) => console.log(response))
+                .catch((error) => console.log(error))
 
-            this.$router.push('/admin/acomodacoes')
+            //this.$router.push('/admin/acomodacoes')
         },
-        cadastrarImagem(e){
-            this.imagem = e.target.files[0].name
-        }
-    }
+        uploadImage: async function () {
+            const formData = new FormData()
+            formData.append('UploadImage', this.file)
+            console.log(this.$refs.file.files[0])
+            try {
+                await axios
+                    .post('http://localhost:3000/admin/room/put', formData)
+                    .then((response) => console.log(response.data.message))
+                    .catch((error) => console.log(error.response.data.message))
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        selectFile() {
+            const file = this.$refs.file.files[0]
+            this.file = file
+        },
+    },
 }
 </script>
 
 <style scoped>
-.formulario{
+.formulario {
     width: 50%;
     margin: 0 auto;
     margin: 3% auto 0 auto;
@@ -95,7 +123,7 @@ export default{
     background: #063f57;
     border-radius: 10px;
 }
-.formulario p{
+.formulario p {
     color: #a35700;
     font-weight: bold;
     line-height: 50px;
@@ -104,26 +132,28 @@ export default{
     margin-right: 10px;
     cursor: pointer;
 }
-.formulario h2{
+.formulario h2 {
     color: #ffffff;
     text-align: center;
 }
-.formulario label{
+.formulario label {
     width: 100%;
     display: block;
     color: #ffffff;
     margin: 5px 0;
 }
-.formulario input, .formulario select, textarea{
+.formulario input,
+.formulario select,
+textarea {
     width: 100%;
     height: 40px;
     font-size: 1rem;
     padding-left: 10px;
 }
-textarea{
+textarea {
     height: 60px;
 }
-.formulario button{
+.formulario button {
     font-size: 1rem;
     padding: 10px 30px;
     background: #a35700;
