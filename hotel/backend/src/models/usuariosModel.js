099664301b921975ea308124a7f8a1038c91838e
Hewerton <hewerton.fl@gmail.com>
2023-04-07 22:37:28 -0300

    aa

--- hotel/backend/src/models/usuariosModel.js
+++ hotel/backend/src/models/usuariosModel.js
@@ -1,4 +1,4 @@
-const conexao = require('../database/conexao')
+const conexao = require('../database/conexao.js')
 
 const listarUsuarios = async () => {
     try {
@@ -6,8 +6,10 @@ const listarUsuarios = async () => {
         const [rows] = await conn.query(
             'SELECT * FROM usuario ORDER BY id_usuario desc'
         )
+        console.log(rows)
         return rows
     } catch (error) {
+        console.log(error)
         return error
     }
 }
@@ -15,7 +17,9 @@ const listarUsuarios = async () => {
 const listarUsuario = async (id) => {
     try {
         const conn = await conexao()
-        const [rows] = await conn.query(`SELECT * FROM usuario WHERE id_usuario = ${id}`)
+        const [rows] = await conn.query(
+            `SELECT * FROM usuario WHERE id_usuario = ${id}`
+        )
         return rows
     } catch (error) {
         return error
@@ -25,29 +29,48 @@ const listarUsuario = async (id) => {
 const cadastrarUsuario = async (dados) => {
     try {
         const conn = await conexao()
-        const values = [dados.nome, dados.sobrenome, dados.email, dados.nivel, dados.status, dados.senha]
-        const [rows] = await conn.query(`INSERT INTO usuario (nome, sobrenome, email, nivel, status, senha) VALUES (?,?,?,?,?,?)`, values)
+        const values = [
+            dados.nome,
+            dados.sobrenome,
+            dados.email,
+            dados.nivel,
+            dados.status,
+            dados.senha,
+        ]
+        const [rows] = await conn.query(
+            `INSERT INTO usuario (nome, sobrenome, email, nivel, status, senha) VALUES (?,?,?,?,?,?)`,
+            values
+        )
     } catch (error) {
         return error
-    }  
+    }
 }
 
 const loginUsuario = async (email) => {
     try {
         const conn = await conexao()
         const values = [email]
-        const [rows] = await conn.query(`SELECT * FROM usuario WHERE email=?`, values)
+        const [rows] = await conn.query(
+            `SELECT * FROM usuario WHERE email=?`,
+            values
+        )
         return rows
     } catch (error) {
         return error
-    }  
+    }
 }
 
-const atualizarUsuario = async (id, {nome, sobrenome, email, nivel, status, senha}) => {
+const atualizarUsuario = async (
+    id,
+    { nome, sobrenome, email, nivel, status, senha }
+) => {
     try {
         const conn = await conexao()
         const values = [nome, sobrenome, email, nivel, status, senha]
-        await conn.query(`UPDATE usuario SET nome=?, sobrenome=?, email=?, nivel=?, status=?, senha=? WHERE id_usuario = ${id}`, values)
+        await conn.query(
+            `UPDATE usuario SET nome=?, sobrenome=?, email=?, nivel=?, status=?, senha=? WHERE id_usuario = ${id}`,
+            values
+        )
         conn.end()
     } catch (error) {
         return error
@@ -66,12 +89,12 @@ const inativarUsuario = async (id) => {
         return error
     }
 }
-
+listarUsuarios()
 module.exports = {
     listarUsuarios,
     listarUsuario,
     cadastrarUsuario,
     loginUsuario,
     inativarUsuario,
-    atualizarUsuario
+    atualizarUsuario,
 }
